647576574d7c976f3d9cf37142e997657dd3750c
makkabosu <lambddri@gmail.com>
2014-10-14 01:23:37 +0900

    add numbers-only

--- test.scm
+++ test.scm
@@ -53,11 +53,7 @@
    [else (find pred (cdr lis))]))
 
 (define (length2 lis)
-  (define (length-rec lis n)
-    (if (null? lis)
-	n
-	(length-rec (cdr lis) (+ n 1))))
-  (length-rec lis 0))
+  (fold (lambda (a b) (+ b 1)) 0 lis))
 
 (define (filter2 pred lis)
   (cond 
@@ -65,3 +61,44 @@
    ((pred (car lis)) (cons (car lis)
 			   (filter2 pred (cdr lis))))
    (else (filter2 pred (cdr lis)))))
+
+(define (max-number lis)
+  (if (null? lis)
+      (error "max-number needs at least one number")
+      (fold (lambda (a b) (if (> a b) a b)) (car lis) (cdr lis))))
+
+(define (print-elements lis)
+  (fold (lambda (a b) (print a)) #f lis))
+
+(define (tree-walk walker proc tree)
+  (walker (lambda (elt)
+	    (if (list? elt)
+		(tree-walk walker proc elt)
+		(proc elt)))
+	  tree))
+
+;;(define (reverse-for-each proc lis)
+;;  (for-each proc (reverse lis)))
+
+;;(define (reverse-map proc lis)
+;;  (map proc (reverse lis)))
+
+(define (reversed walker)
+  (lambda (proc lis)
+    (walker proc (reverse lis))))
+
+(define reverse-for-each (reversed for-each))
+
+(define reverse-map (reversed map))
+
+(define (for-each-numbers proc lis)
+  (for-each proc 
+	    (filter number? lis)))
+
+(define (map-numbers proc lis)
+  (map proc
+       (filter number? lis)))
+
+(define (numbers-only walker)
+  (lambda (proc lis)
+    (walker proc (filter number? lis))))
